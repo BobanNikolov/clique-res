e2d3f6514b2a7a72de640726aa4d1d5742bcfcf3
BobanNikolov <boban.nikolov@students.finki.ukim.mk>
2023-08-06 21:03:36 +0200

    date picker home page done

--- src/main/java/com/example/cliqueres/web/HomeController.java
+++ src/main/java/com/example/cliqueres/web/HomeController.java
@@ -1,11 +1,14 @@
 package com.example.cliqueres.web;
 
+import com.example.cliqueres.domain.Event;
+import com.example.cliqueres.domain.User;
 import com.example.cliqueres.service.EventService;
 import com.example.cliqueres.service.ReservationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,9 +18,14 @@ public class HomeController {
     private final EventService eventService;
     private final ReservationService reservationService;
     @GetMapping
-    public String getHome(Model model) {
-        model.addAttribute("bodyContent", "home");
-//        model.addAttribute("upcomingEvents", eventService.findAll());
+    public String main(Model model) {
+        model.addAttribute("event", new Event());
         return "home";
+    }
+
+    @PostMapping
+    public String save(Event event, Model model) {
+        model.addAttribute("event", event);
+        return "saved";
     }
 }
